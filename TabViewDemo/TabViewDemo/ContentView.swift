ca69bdc1c0f1ed47e55515835aa794e4421f07cd
Caumannerman <uigohiiming2@gmail.com>
2022-02-24 21:26:04 +0900

    Add TabViewDemo Example

---
+++ TabViewDemo/TabViewDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabViewDemo
+//
+//  Created by 양준식 on 2022/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabViewDemo/TabViewDemo/TabViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewDemoApp.swift
+//  TabViewDemo
+//
+//  Created by 양준식 on 2022/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TabViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
